2baa9e25b7e4afba4173ce15773f387c9d23b0fe
Ruslan Valeev <78922271+FruitEnjoyer@users.noreply.github.com>
2025-01-05 23:49:03 +0300

    Delete doxygen directory

--- doxygen/html/search/all_0.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['ant_0',['Ant',['../classant_1_1Ant.html',1,'ant::Ant'],['../classant_1_1Ant.html#abcf9a80a7d5f92ca65775559345fd4e6',1,'ant::Ant::Ant()']]],
-  ['ant_2ecpp_1',['ant.cpp',['../ant_8cpp.html',1,'']]]
-];
--- doxygen/html/search/files_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['ant_2ecpp_3',['ant.cpp',['../ant_8cpp.html',1,'']]]
-];
